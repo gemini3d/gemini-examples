c052d72ccdf28103f0c5ee13bffabbd2433e9eae
Matthew Zettergren <mattzett@gmail.com>
2019-06-26 12:23:07 -0400

    minor updates to GDI_large

--- initialize/GDI_periodic_highres_large/README.md
+++ initialize/GDI_periodic_highres_large/README.md
@@ -6,4 +6,6 @@ The background density (given from the equilibrium simulation RISR_eq) is about 
 
 May want to consider adding a stretched grid in x2 to eliminate potential boundary affects on the simulations.
 
+The grid is such that it can be divided into 20x18 subdomains so that it runs well on VEGA.  This is results in a grid of 64x1540x1062 - just over 100M grid points.  
+
 
--- initialize/GDI_periodic_highres_large/model_setup_perturb_GDI.m
+++ initialize/GDI_periodic_highres_large/model_setup_perturb_GDI.m
@@ -3,6 +3,7 @@ gemini_root = [cwd, filesep, '../../../GEMINI'];
 addpath([gemini_root, filesep, 'script_utils'])
 addpath([gemini_root, filesep, 'vis'])
 
+
 %READ IN THE SIMULATION INFORMATION
 ID='~/zettergmdata/simulations/input/GDI_periodic_highres_fileinput_large/';
 xg=readgrid(ID);
@@ -14,32 +15,6 @@ filebase='GDI_periodic_highres_fileinput_large';
 filename=[filebase,'_ICs.dat'];
 [ne,v1,Ti,Te,ns,vs1,Ts,simdate]=loadframe3Dcurvnoelec(direc,filename);
 lsp=size(ns,4);
-
-%DEFINE A PERTURBATION AND CHANGE THE INITIAL CONDITIONS
-%{
-%%GDI nonperiodic
-sigx2=30e3;
-meanx3=0e3;
-sigx3=30e3;
-meanx2=-30e3;
-
-for isp=1:lsp
-  for ix3=1:xg.lx(3)
-    for ix2=1:xg.lx(2)
-      amplitude=rand(xg.lx(1),1);
-      amplitude=0.1*amplitude;
-      nsperturb(:,ix2,ix3,isp)=ns(:,ix2,ix3,isp)+ ...                                           %original data
-                amplitude.*ns(:,ix2,ix3,isp)+ ...                                    %noise
-                7.5d0*ns(:,ix2,ix3,isp).*exp(-1d0*(xg.x2(2+ix2)-meanx2).^18/2d0/sigx2.^18).* ...
-                exp(-1d0*(xg.x3(2+ix3)-meanx3).^18/2d0/sigx3.^18);    %patch, note offset in the x2 index!!!!
-    end
-  end
-end
-%}
-
-
-%ADD ON SOME DENSITY TO BACKGROUND
-%ns=6.25*ns;
 
 
 %%GDI EXAMPLE (PERIODIC)
@@ -62,29 +37,7 @@ for isp=1:lsp
     nsperturb(:,ix2,:,isp)=scalefact*nsperturb(:,ix2,:,isp);
   end
 end
-%nsperturb=max(nsperturb,1e4);
-
-
-%%KHI EXAMPLE
-%{
-v0=500d0;
-vn=500d0;
-voffset=100d0;
-
-sigx2=770e0;      %from Keskinen, 1988 growth rate formulas
-meanx3=0e3;
-sigx3=20e3;
-meanx2=0e0;
-
-for isp=1:lsp
-  for ix2=1:xg.lx(2)
-    amplitude=rand(xg.lx(1),1,xg.lx(3));
-    amplitude=0.025*amplitude;
-    nsperturb(:,ix2,:,isp)=ns(:,ix2,:,isp).*(v0+vn+voffset)./(-v0*tanh((xg.x2(2+ix2))/sigx2)+vn+voffset)+ ...
-                          amplitude.*10e0.*ns(:,ix2,:,isp);
-  end
-end
-%}
+nsperturb=max(nsperturb,1e4);
 
 
 %WRITE OUT THE RESULTS TO A NEW FILE
