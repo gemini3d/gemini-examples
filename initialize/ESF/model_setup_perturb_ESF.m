ed75e9251df46f27d676519f9a7e8ba79cbf20d6
Matthew Zettergren <mattzett@users.noreply.github.com>
2019-12-20 09:38:58 -0500

    ESF perturbation script complete

--- initialize/ESF/model_setup_perturb_ESF.m
+++ initialize/ESF/model_setup_perturb_ESF.m
@@ -6,7 +6,7 @@ addpath([gemini_root, filesep, 'vis']);
 
 %% READ IN THE SIMULATION INFORMATION
 ID=[gemini_root,'/../simulations/input/ESF_medres/'];
-xg=readgrid([ID]);
+xg=readgrid([ID,'inputs/']);
 x1=xg.x1(3:end-2); x2=xg.x2(3:end-2); x3=xg.x3(3:end-2);
 lx1=xg.lx(1); lx2=xg.lx(2); lx3=xg.lx(3);
 
@@ -19,4 +19,43 @@ filename=[filebase,'_ICs.dat'];
 lsp=size(ns,4);
 
 
-%%
+%% Define a shape function for a perturbation on this grid
+alt=xg.alt;
+mlat=90-xg.theta*180/pi;
+mlon=xg.phi*180/pi;
+mlonmean=mean(mlon(:));
+mlatmean=0;
+altmean=300e3;
+sigmlon=0.5;
+sigmlat=5;
+sigalt=15e3;
+shapefn=exp(-(alt-altmean).^2/2/sigalt^2).*exp(-(mlon-mlonmean).^2/2/sigmlon^2).*exp(-(mlat-mlatmean).^2/2/sigmlat^2);
+n1=ns(:,:,:,1);
+n1perturb=n1-shapefn*0.25.*n1;
+
+
+%% Visualize
+ix1=lx1/2;
+
+figure;
+subplot(121);
+imagesc(squeeze(mlon(ix1,1,:)),squeeze(alt(ix1,:,1)),squeeze(n1(ix1,:,:)))
+axis xy;
+
+subplot(122);
+imagesc(squeeze(mlon(ix1,1,:)),squeeze(alt(ix1,:,1)),squeeze(n1perturb(ix1,:,:)))
+axis xy;
+
+
+%% Create a modified density variable for output
+nsperturb=ns;
+nsperturb(:,:,:,1)=n1perturb;
+nsperturb(:,:,:,7)=sum(nsperturb(:,:,:,1:6),4);
+
+
+%% WRITE OUT THE RESULTS TO A NEW FILE
+outdir=ID;
+dmy=[simdate(3),simdate(2),simdate(1)];
+UTsec=simdate(4)*3600;
+writedata(dmy,UTsec,nsperturb,vs1,Ts,outdir,[filebase,'_perturb']);
+
