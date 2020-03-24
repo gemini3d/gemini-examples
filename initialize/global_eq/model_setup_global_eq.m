617a3ce29efc754f85a9739e617167139ef7180f
Matthew Zettergren <mattzett@gmail.com>
2020-03-23 20:00:46 -0400

    updated global_eq example

--- initialize/global_eq/model_setup_global_eq.m
+++ initialize/global_eq/model_setup_global_eq.m
@@ -1,15 +1,17 @@
-cwd = fileparts(mfilename('fullpath'));
-gemini_root = [cwd, filesep, '../../../GEMINI'];
-addpath([gemini_root, filesep, 'script_utils'])
-addpath([gemini_root, filesep, 'setup/gridgen'])
-addpath([gemini_root, filesep, 'setup'])
+%cwd = fileparts(mfilename('fullpath'));
+%gemini_root = [cwd, filesep, '../../../GEMINI'];
+%addpath([gemini_root, filesep, 'script_utils'])
+%addpath([gemini_root, filesep, 'setup/gridgen'])
+%addpath([gemini_root, filesep, 'setup'])
+%User must run gemini-matlab repo script setup.m to set paths
 
-%A HIGHRES TOHOKU INIT GRID
-dtheta=10;
-dphi=365-365/128;
+
+%% A HIGHRES TOHOKU INIT GRID
+dtheta=15;
 lp=256;
 lq=384;
 lphi=128;
+dphi=365-365/lphi;
 altmin=80e3;
 glat=42.45;
 glon=143.4;
@@ -17,26 +19,31 @@ gridflag=1;
 flagsource=1;
 
 
-%MATLAB GRID GENERATION
-xg=makegrid_tilteddipole_3D(dtheta,dphi,lp,lq,lphi,altmin,glat,glon,gridflag);
+%% MATLAB GRID GENERATION
+if (~exist('xg'))
+    xg=makegrid_tilteddipole_3D(dtheta,dphi,lp,lq,lphi,altmin,glat,glon,gridflag);
+end %if
 
 
-%GENERATE SOME INITIAL CONDITIONS FOR A PARTICULAR EVENT, THESE ACTUALLY DON'T MATTER MUCH SO YOU CAN MAKE UP STUFF
-UT=5.75;
-dmy=[11,3,2011];
-activ=[120,120,25];
+%% Plot grid
+flagsource=0;
+neuinfo=struct();
+plot_mapgrid(xg,flagsource,neuinfo);
 
 
-%USE OLD CODE FROM MATLAB MODEL
-nmf=5e11;
-nme=2e11;
-[ns,Ts,vsx1]=eqICs3D(xg,UT,dmy,activ,nmf,nme);    %note that this actually calls msis_matlab - should be rewritten to include the neutral module form the fortran code!!!
+%% GENERATE SOME INITIAL CONDITIONS FOR A PARTICULAR EVENT, THESE ACTUALLY DON'T MATTER MUCH SO YOU CAN MAKE UP STUFF
+p.UTsec0=5.75*3600;
+p.ymd=[2011,3,11];
+p.activ=[120,120,25];
+p.nmf=5e11;
+p.nme=2e11;
+[ns,Ts,vsx1]=eqICs3D(p,xg);    %note that this actually calls msis_matlab - should be rewritten to include the neutral module form the fortran code!!!
 
 
-%WRITE THE GRID AND INITIAL CONDITIONS
-outdir=[gemini_root,'/../simulations/input/global_eq/']
-simlabel='global_eq'
-writegrid(xg,outdir);
-time=UT*3600;   %doesn't matter for input files
-writedata(dmy,time,ns,vsx1,Ts,outdir,simlabel);
+%% WRITE THE GRID AND INITIAL CONDITIONS
+p.simdir='../../../simulations/input/global_eq';
+p.format='raw';
+p.realbits=64;
+writegrid(p,xg);
+writedata(p.ymd,p.UTsec0,ns,vsx1,Ts,p.simdir,'raw',64);
 
