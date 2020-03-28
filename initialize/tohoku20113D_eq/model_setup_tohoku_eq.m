4d7c0a72c06d9b9926f960f9e52a2eb7a098baac
Matthew Zettergren <mattzett@gmail.com>
2020-03-28 14:02:36 -0400

    updated tohoku eq eample

--- initialize/tohoku20113D_eq/model_setup_tohoku_eq.m
+++ initialize/tohoku20113D_eq/model_setup_tohoku_eq.m
@@ -1,15 +1,16 @@
-cwd = fileparts(mfilename('fullpath'));
-gemini_root = [cwd, filesep, '../../../GEMINI'];
-addpath([gemini_root, filesep, 'script_utils'])
-addpath([gemini_root, filesep, 'setup/gridgen'])
-addpath([gemini_root, filesep, 'setup'])
+% cwd = fileparts(mfilename('fullpath'));
+% gemini_root = [cwd, filesep, '../../../GEMINI'];
+% addpath([gemini_root, filesep, 'script_utils'])
+% addpath([gemini_root, filesep, 'setup/gridgen'])
+% addpath([gemini_root, filesep, 'setup'])
 
-%A HIGHRES TOHOKU INIT GRID
+
+%% A HIGHRES TOHOKU INIT GRID
 dtheta=10;
 dphi=15;
-lp=100;
-lq=500;
-lphi=25;
+lp=128;
+lq=384;
+lphi=32;
 altmin=80e3;
 glat=42.45;
 glon=143.4;
@@ -17,26 +18,25 @@ gridflag=1;
 flagsource=1;
 
 
-%MATLAB GRID GENERATION
-xg=makegrid_tilteddipole_3D(dtheta,dphi,lp,lq,lphi,altmin,glat,glon,gridflag);
+%% MATLAB GRID GENERATION
+if (~exist('xg'))
+    xg=makegrid_tilteddipole_3D(dtheta,dphi,lp,lq,lphi,altmin,glat,glon,gridflag);
+end %if
 
 
-%GENERATE SOME INITIAL CONDITIONS FOR A PARTICULAR EVENT, THESE ACTUALLY DON'T MATTER MUCH SO YOU CAN MAKE UP STUFF
-UT=5.75;
-dmy=[11,3,2011];
-activ=[120,120,25];
+%% GENERATE SOME INITIAL CONDITIONS FOR A PARTICULAR EVENT, THESE ACTUALLY DON'T MATTER MUCH SO YOU CAN MAKE UP STUFF
+p.UTsec0=5.75;
+p.ymd=[2011,3,11];
+p.activ=[120,120,25];
+p.nmf=5e11;
+p.nme=2e11;
+[ns,Ts,vsx1]=eqICs3D(p,xg);    %note that this actually calls msis_matlab - should be rewritten to include the neutral module form the fortran code!!!
 
 
-%USE OLD CODE FROM MATLAB MODEL
-nmf=5e11;
-nme=2e11;
-[ns,Ts,vsx1]=eqICs3D(xg,UT,dmy,activ,nmf,nme);    %note that this actually calls msis_matlab - should be rewritten to include the neutral module form the fortran code!!!
+%% WRITE THE GRID AND INITIAL CONDITIONS
+p.simdir='../../../simulations/input/tohoku20113D_eq';
+p.format='raw';
+p.realbits=64;
 
-
-%WRITE THE GRID AND INITIAL CONDITIONS
-outdir='~/'
-simlabel='tohoku_eq'
-writegrid(xg,outdir,simlabel);
-time=UT*3600;   %doesn't matter for input files
-writedata(dmy,time,ns,vsx1,Ts,outdir,simlabel);
-
+writegrid(p,xg);
+writedata(p.ymd,p.UTsec0,ns,vsx1,Ts,p.simdir,'raw',64);
