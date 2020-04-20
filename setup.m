59646ee1ca889e6402085f367de18f2be96aa69d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-04-20 10:28:46 -0400

    git clone gemini-matlab if needed

--- setup.m
+++ setup.m
@@ -1,2 +1,9 @@
 cwd = fileparts(mfilename('fullpath'));
-run([cwd, '/../gemini-matlab/setup.m'])
+gemini_matlab = [cwd, '/../gemini-matlab'];
+if ~isfolder(gemini_matlab)
+  cmd = ['git -C ',cwd,'/../ clone https://github.com/gemini3d/gemini-matlab'];
+  disp(cmd)
+  ret = system(cmd);
+  assert(ret==0, 'problem downloading Gemini Matlab functions')
+end
+run([gemini_matlab, '/setup.m'])
