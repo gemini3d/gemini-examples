21641dee68b4ccb560578d1b7c2fbaa52c22a22b
Matthew Zettergren <mattzett@gmail.com>
2020-05-13 15:53:33 -0400

    working on README for auroral arcs examples

---
+++ initialize/arcs/README.md
@@ -0,0 +1,48 @@
+# Auroral Arcs Examples
+
+This example shows an application of the GEMINI model to simulate a 3D auroral arc.  The simulation is driven by application of a field-aligned currents at the top boundary, as well as precipitation particles.  Steps to run and alter this example are given below:  
+
+
+## GEMINI Installation
+
+1. Before attempting to run this example you will need to do a full GEMINI installation as described in the README for the core [GEMINI](https://github.com/gemini3d/gemini) repository.  You will also need to install the [gemini-examples](https://github.com/gemini3d/gemini-examples) repository and the [gemini-matlab](https://github.com/gemini3d/gemini-matlab) repository.
+
+
+## Equilibrium Simulation
+
+2. Generate an equilibrium simulation for this example using the [ARCs\_eq example](https://github.com/gemini3d/gemini-examples/tree/master/initialize/arcs_eq).  Alternatively one may request input data from one of the repo maintainers, but there is not gaurantee of a timely response and/or the input data may be quite large and unwieldy to transfer across a network.  
+
+
+## Disturbance Simulation Setup
+
+3. Define a new grid using parameters in the config.nml file in this directory.  The .nml file can be edited to adjust the grid extent and resolution (number of grid points), grid center location, and local geomagnetic field inclination.  
+
+4. Define parameters in the ```config.nml``` file for the field-aligned boundary currents.  The peak current density and width of the current distribution can be adjusted via the parameters:
+	
+	```
+	Jtarg = 30e-6                  ! max field aligned current A/m^2 at the top boundary
+    Efield_fracwidth2 = 0.15.      ! fraction of the grid in x2 spanned by the current density perturbation
+    Efield_fracwidth3 = 0.025.     ! fraction of the grid in x3 "
+	```
+	In addition to these basic parameters, one must also specify the shape of the current density pattern imposed at the top boundary.  This can be done by directly editing the .  Future releases will likely wrap this functionality into a user-defined shape function that can be provided to the ```model_setup.m``` script  
+
+	shape parameters for generating the boundary conditions corresponding to the auroral.  This can be done by either directly editing the scripts that specify boundary conditions
+
+5. Define parameters 
+
+6. Run the top-level MATLAB script to generate files for initial and boundary conditions for this simulation.  Navigate to this directory in the MATLAB command window and then execute:  ```>> model_setup('config.nml')```
+You should see a bunch of console output in MATLAB to verify that the grid is being created and HDF5 input files for the fortran code are being written.  
+
+
+## Launching Disturbance Simulation
+
+7. This simulation may be run from the command line by:
+
+	```sh
+	mpirun -np 8 ./gemini.bin path/GDI_periodic_medres/config.ini path/outputdirectory/ -manual_grid 4 2
+	```
+	It will take approximately several hours to complete on a 4-8 core system; It is recommended that one use at least 16-32 cores, if available, to speed the calculations.  It is possible to use up to ~192 cores with this example if you manually specify the mpi split in x2 and x3, e.g.:
+	
+	```
+	mpirun -np 192 ./gemini.bin path/GDI_periodic_medres/config.ini path/outputdirectory/ -manual_grid 16 12
+	```
--- initialize/isinglass_clayton_flight/Efield_tucker.m
+++ initialize/isinglass_clayton_flight/Efield_tucker.m
@@ -1,6 +1,6 @@
 clear all
 close all
-plotflag=0;
+plotflag=1;
 
 
 %% Where to put the output files for GEMINI
@@ -11,13 +11,13 @@ system(['mkdir ',outdir]);
 %% Load Tucker's fits for electric field
 inputdir='~/Dropbox/common/mypapers/ISINGLASS/paper2_finally/';
 load([inputdir,'tucker_reconstructions.mat']);    %this is MZ's decurled version of Robs inputs...
-Xgeomag=permute(xout,[3,1,2]);    %transport so that x is column index
+Xgeomag=permute(xout,[3,1,2]);                    %permute so that x is column index
 Ygeomag=permute(yout,[3,1,2]);
-Exgeomagdat=permute(uE,[3,1,2]);
-Eygeomagdat=permute(vE,[3,1,2]);
+Exgeomagdat=permute(uE,[3,2,1]);                  %why are these ordered differently!!!
+Eygeomagdat=permute(vE,[3,2,1]);
 
 
-%% Trim dataset to exclude boundary artifacts
+%% Trim dataset to exclude boundary artifacts from Tucker's fits
 ledge=5;    %number of edge points to trim off
 Xgeomag=Xgeomag(:,ledge:end-ledge,ledge:end-ledge);
 Ygeomag=Ygeomag(:,ledge:end-ledge,ledge:end-ledge);
@@ -28,28 +28,12 @@ Eygeomagdat=Eygeomagdat(:,ledge:end-ledge,ledge:end-ledge);
 
 %% Convert geomag input to geographic
 Ygeo=zeros(lt,llon,llat);
+Xgeo=zeros(lt,llon,llat);
 for it=1:lt
-    Xgeomagtmp=squeeze(Xgeomag(1,:,:));
+    Xgeomagtmp=squeeze(Xgeomag(1,:,:));    %really need the grid to not change with time, appears not to
     Ygeomagtmp=squeeze(Ygeomag(1,:,:));
     [Ygeo(it,:,:),Xgeo(it,:,:)]=geomag2geog(pi/2-Ygeomagtmp*pi/180,Xgeomagtmp*pi/180);
 end %for
-
-
-%% Load Matt's GEMINI grid
-%load([inputdir,'/isinglass_clayton_grid.mat']);
-% 
-% 
-% %% Field and position data from Rob's file
-% datapath='./';
-% Exgeomagdat=outu;
-% Eygeomagdat=outv;
-% 
-% for i=1:1:length(outt)
-%     Xgeo(i,:,:)=(squeeze(glon(100,1:2:end-1,1:2:end-1)))';    % Geo lon
-%     Ygeo(i,:,:)=(squeeze(glat(100,1:2:end-1,1:2:end-1)))';    % Geo lat
-% end
-% [lt,llon,llat]=size(Exgeomagdat);
-% clear glon glat
 
 
 %AFAIK previous code's purpose is to put the data into the arrays Xgeo
@@ -72,211 +56,31 @@ griddir='~/simulations/input/isinglass_clayton_flight/';    %have the prep code 
 Efield2input(t,Xgeo,Ygeo,Exgeomagdat,Eygeomagdat,griddir,outdir);
 
 
-% %% CONVERT POSITIONS TO GEOMAGNETIC USING SAME ALGORITHM AS SIMULATION
-% glat=Ygeo;
-% glon=Xgeo;
-% gloncorrected=glon;
-% 
-% 
-% %% FIND MLAT,MLON POSITION OF INPUT DATA
-% fprintf('Interpolating...\n')
-% theta=zeros(llon,llat,lt);
-% phi=zeros(llon,llat,lt);
-% for it=1:lt
-%     for ilat=1:llat
-%         for ilon=1:llon
-%             [thetatmp,phitmp]=geog2geomag(glat(it,ilat,ilon),glon(it,ilat,ilon));
-%             theta(ilon,ilat,it)=thetatmp;
-%             phi(ilon,ilat,it)=phitmp;
-%         end
-%     end
-% end
-% 
-% 
-% %% NUMBER OF LAT/LON POINTS FOR OUTPUT DATAS
-% llat2=256;
-% llon2=256;
-% % mlatdat=90-theta*180/pi;
-% % mlondat=phi*180/pi;
-% % mlatmin=min(mlatdat(:));
-% % mlatmax=max(mlatdat(:));
-% % mlat=linspace(mlatmin,mlatmax,llat2);   %use same number of points as original data
-% % mlonmin=min(mlondat(:));
-% % mlonmax=max(mlondat(:));
-% % mlon=linspace(mlonmin,mlonmax,llon2);
-% % [MLAT,MLON]=meshgrid(mlat,mlon);    %mlon goes down the vertical dimension of the matrices in this script
-% MLAT = squeeze(mlat(1,:,:));
-% MLON = squeeze(mlon(1,:,:));
-% mlat = linspace(min(min(MLAT)),max(max(MLAT)),llat2);
-% mlon = linspace(min(min(MLON)),max(max(MLON)),llon2);
-% theta=pi/2-mlat*pi/180;    %don't sort since we are making a glon,glat grid out of this...
-% phi=mlon*pi/180;
-% 
-% [THETA,PHI]=meshgrid(theta,phi);    %because we've arranged the data as lon,lat.
-% [GLAT,GLON]=geomag2geog(THETA,PHI);
-% 
-% 
-% %% DO SPATIAL INTERPOLATIONS
-% Exgeomag=zeros(llon2,llat2,lt);
-% Eygeomag=zeros(llon2,llat2,lt);
-% for it=1:lt
-%   Extmp=squeeze(Exgeomagdat(it,:,:));
-%   glontemp = squeeze(gloncorrected(it,:,:))';
-%   glattemp = squeeze(glat(it,:,:))';
-%   F=TriScatteredInterp(glontemp(:),glattemp(:),Extmp(:));   %the source data are not on a plaid grid here...
-%   Extmp=F(GLON(:),GLAT(:));
-%   Exgeomag(:,:,it)=reshape(Extmp,[llon2,llat2]);
-%   Eytmp=squeeze(Eygeomagdat(it,:,:));
-%   F=TriScatteredInterp(glontemp(:),glattemp(:),Eytmp(:));
-%   Eygeomag(:,:,it)=reshape(F(GLON(:),GLAT(:)),[llon2,llat2]);
-% end
-% 
-% 
-% %DEBUG PLOTTING OF ORIGINAL DATA
-% if (plotflag)
-%     plotdir='./plotsE/';
-%     system(['mkdir ',plotdir]);
-%     figure;
-%     set(gcf,'PaperPosition',[0 0 8.5 3.5]);
-%     for it=1:lt
-%         clf;
-%         subplot(131);
-%         quiver(gloncorrected,glat,Exgeog(:,:,it),Eygeog(:,:,it));
-%         xlabel('geographic lon.');
-%         ylabel('geographic lat.');
-%         title(datestr(datenum(expdate(it,:))));
-%         
-%         subplot(132);
-%         quiver(GLON,GLAT,Exgeomag(:,:,it),Eygeomag(:,:,it));
-%         xlabel('geog. lon. (resamp.)');
-%         ylabel('geog. lat. (resamp.)');
-%         title(datestr(datenum(expdate(it,:))));
-%         
-%         subplot(133);
-%         %    quiver(MLON,MLAT,Exgeomag(:,:,it),Eygeomag(:,:,it));
-%         quiver(MLON',MLAT',Exgeomag(:,:,it)',Eygeomag(:,:,it)');
-%         xlabel('geomagnetic lon.');
-%         ylabel('geomagnetic lat.');
-%         title(datestr(datenum(expdate(it,:))));
-%         
-%         UTsec=expdate(it,4)*3600+expdate(it,5)*60+expdate(it,6);
-%         ymd=expdate(it,1:3);
-%         filename=datelab(ymd,UTsec);
-%         filename=[plotdir,filename,'.png']
-%         
-%         print('-dpng',filename,'-r300')
-%     end
-%     close all;
-% end %if
-% 
-% 
-% %% NEED TO SAMPLE DATA ON A UNIFORM TEMPORAL GRID FOR THE MODEL
-% fprintf("Resampling data...\n")
-% %samplerate=min(diff(t));    %sampling rate in days, this uses the min from the dataset
-% % samplerate=10/86400;    %sampling rate in days
-% samplerate=1/86400;
-% sampleratesec=samplerate*86400;
-% sampleratesec=round(sampleratesec);
-% samplerate=sampleratesec/86400;
-% %outputt=min(t):samplerate:max(t);    %spans data set
-% %TOIstartdate=[2017,03,02,28200/3600,0,0];    %pick start and end times for the field data times of interest
-% %TOIenddate=[2017,03,02,28797/3600,0,0];
-% TOIstartdate=[2017,03,02,outt(1),0,0];    %pick start and end times for the field data times of interest
-% TOIenddate=[2017,03,02,outt(length(outt)),0,0];
-% TOIstartt=datenum(TOIstartdate);
-% TOIendt=datenum(TOIenddate);
-% outputt=TOIstartt:samplerate:TOIendt+samplerate;
-% outputdate=datevec(outputt);
-% ltout=numel(outputt);
-% Exit=zeros(llon2,llat2,ltout);
-% Eyit=zeros(llon2,llat2,ltout);
-% for ilat=1:llat2
-%    for ilon=1:llon2
-%        Exhere=squeeze(Exgeomag(ilon,ilat,:));
-%        inds=find(isnan(Exhere));
-%        Exhere(inds)=0;    %assumes the simulation is not encapsulating the entire domain of the electric field data
-%        Eyhere=squeeze(Eygeomag(ilon,ilat,:));
-%        inds=find(isnan(Eyhere));
-%        Eyhere(inds)=0;
-%        Exi=interp1(t,Exhere,outputt);
-%        inds=find(isnan(Exi));
-%        Exi(inds)=0;
-%        Eyi=interp1(t,Eyhere,outputt);
-%        inds=find(isnan(Eyi));
-%        Eyi(inds)=0;       
-%        Exit(ilon,ilat,:)=reshape(Exi,[1 1 ltout]);
-%        Eyit(ilon,ilat,:)=reshape(Eyi,[1 1 ltout]);       
-%    end
-% end
-% 
-% 
-% % % PLOT THE INTERPOLATED DATA AS A CHECK
-% % figure;
-% % % for it=1:ltout
-% % for it=1:length(Exit(1,1,:))
-% %     clf;
-% %     quiver(MLON',MLAT',Exit(:,:,it)',Eyit(:,:,it)');
-% %     xlabel('geomagnetic lon.');
-% %     ylabel('geomagnetic lat.');
-% %     title(datestr(datenum(outputdate(it,:))));
-% % 
-% % pause()
-% % 
-% % %     UTsec=outputdate(it,4)*3600+outputdate(it,5)*60+outputdate(it,6);
-% % %     ymd=outputdate(it,1:3);
-% % %     filename=datelab(ymd,UTsec);
-% % %     filename=['./',filename,'.png']
-% % %     
-% % %     print('-dpng',filename,'-r300')
-% % end
-% 
-% 
-% %% ADD IN THE BOUNDARY CONDITIONS
-% Vminx1it=zeros(llon2,llat2,ltout);
-% Vmaxx1it=zeros(llon2,llat2,ltout);
-% Vminx2it=zeros(llat2,ltout);
-% Vmaxx2it=zeros(llat2,ltout);
-% Vminx3it=zeros(llon2,ltout);
-% Vmaxx3it=zeros(llon2,ltout);
-% 
-% 
-% %% SAVE THESE DATA TO APPROPRIATE FILES - LEAVE THE SPATIAL AND TEMPORAL INTERPOLATION TO THE
-% %FORTRAN CODE IN CASE DIFFERENT GRIDS NEED TO BE TRIED.  THE EFIELD DATA DO NOT NEED TO BE SMOOTHED.
-% fprintf("Generating GEMINI inputs files...\n");
-% filename=[outdir,'simsize.dat'];
-% fid=fopen(filename,'wb');
-% fwrite(fid,llon2,'integer*4');
-% fwrite(fid,llat2,'integer*4');
-% fclose(fid);
-% filename=[outdir,'simgrid.dat'];
-% fid=fopen(filename,'w');
-% fwrite(fid,mlon,'real*8');
-% fwrite(fid,mlat,'real*8');
-% fclose(fid);
-% 
-% flagdirich=1;   %if 0 data is interpreted as FAC, else we interpret it as potential
-% for it=1:ltout
-%     UTsec=outputdate(it,4)*3600+outputdate(it,5)*60+fix(outputdate(it,6));
-%     ymd=outputdate(it,1:3);
-%     filename=datelab(ymd,UTsec);
-%     %filename='2017_4_6_';
-%     filename=[outdir,filename,'.dat']
-%     fid=fopen(filename,'w');
-% 
-%     fwrite(fid,flagdirich,'real*8');
-%     fwrite(fid,Exit(:,:,it),'real*8');
-%     fwrite(fid,Eyit(:,:,it),'real*8');
-%     fwrite(fid,Vminx1it(:,:,it),'real*8');
-%     fwrite(fid,Vmaxx1it(:,:,it),'real*8');
-%     fwrite(fid,Vminx2it(:,it),'real*8');
-%     fwrite(fid,Vmaxx2it(:,it),'real*8');
-%     fwrite(fid,Vminx3it(:,it),'real*8');
-%     fwrite(fid,Vmaxx3it(:,it),'real*8');
-% 
-%     fclose(fid);
-% end
-% 
-% 
-% %ALSO CREATE A MATLAB OUTPUT FILE FOR GOOD MEASURE
-% %save([outdir,'fields.mat'],'glon','glat','mlon','mlat','GLAT','GLON','MLAT','MLON','Exit','Eyit','Exgeog','Eygeog','Vm*','outputdate');
-% save([outdir,'fields.mat'],'glon','glat','mlon','mlat','GLAT','GLON','MLAT','MLON','Exit','Eyit','Vm*','outputdate');
+%% Visualize input data
+if (plotflag)
+    figure;
+    for it=1:lt
+        xnow=xout(:,1,1);
+        ynow=yout(1,:,1);
+        uEnow=uflow(:,:,it);
+        vEnow=vflow(:,:,it);
+        subplot(121);
+        imagesc(xnow',ynow',uEnow')
+        axis xy;
+        caxis([-0.05,0.05])
+        colorbar('Location','SouthOutside');
+        title(sprintf('Eastward, frame %d',it))
+        subplot(122);
+        imagesc(xnow',ynow',vEnow')
+        axis xy;
+        caxis([-0.05,0.05])      
+        colorbar('Location','SouthOutside');
+        title(sprintf('Northward, frame %d',it))
+        framenum=num2str(it);
+        while (numel(framenum)<2)
+            framenum=['0',framenum];
+        end %while
+        print('-dpng',['~/Downloads/Tucker/',framenum,'.png'])
+    end %for
+end %if
+
