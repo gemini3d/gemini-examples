02a7d7349d581eda1885b2447f127c9aef24690e
Matthew Zettergren <mattzett@gmail.com>
2020-06-17 14:29:29 -0400

    some updates to examples

--- initialize/GDI_periodic_lowres/config.m
+++ initialize/GDI_periodic_lowres/config.m
@@ -8,6 +8,7 @@ cfg = read_config(cwd);
 % should be able to include cfg.x2parms to generate nonuniform grid in
 % x2...
 cfg.x2parms=[200e3,0.5e3,9.5e3,10e3];
+cfg.outdir='~/simulations/GDI_periodic_lowres_test/inputs';
 xg = makegrid_cart_3D(cfg);
 %% Interpolate data to desired grid resolution
 eq2dist(cfg, xg);
