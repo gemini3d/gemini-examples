9f807481d177ea824b3d9db5d694b1c3d0452d51
Matthew Zettergren <mattzett@gmail.com>
2020-06-18 10:48:59 -0400

    added noiselike perturbations to ESF

--- initialize/ESF/model_setup_interp_ESF.m
+++ initialize/ESF/model_setup_interp_ESF.m
@@ -11,9 +11,9 @@
 
 
 %% Parameters for creating input files from given equilibrium run
-p.eqdir='~/zettergmdata/simulations/ESF_eq/';
+p.eqdir='~/simulations/ESF_eq/';
 p.realbits=64;
-p.outdir='~/zettergmdata/simulations/ESF_medres/inputs';
+p.outdir='~/simulations/ESF_medres_noise/inputs';
 p.nml='./config.nml';
 p.file_format='h5';
 
--- initialize/ESF/model_setup_perturbnoise_ESF.m
+++ initialize/ESF/model_setup_perturbnoise_ESF.m
@@ -5,7 +5,7 @@ gemini_root = [cwd, filesep, '../../../GEMINI'];
 
 
 %% READ IN THE SIMULATION INFORMATION
-ID=[gemini_root,'/../simulations/ESF_medres/inputs/'];
+ID=[gemini_root,'/../simulations/ESF_medres_noise/inputs/'];
 xg=readgrid([ID]);
 x1=xg.x1(3:end-2); x2=xg.x2(3:end-2); x3=xg.x3(3:end-2);
 lx1=xg.lx(1); lx2=xg.lx(2); lx3=xg.lx(3);
@@ -28,38 +28,20 @@ simdate=dat.simdate;
 lsp=size(ns,4);
 
 
-%% Define a shape function for a perturbation on this grid
-alt=xg.alt;
-mlat=90-xg.theta*180/pi;
-mlon=xg.phi*180/pi;
-mlonmean=mean(mlon(:));
-mlatmean=0;
-altmean=300e3;
-sigmlon=0.25;
-sigmlat=2.5;
-sigalt=15e3;
-shapefn=exp(-(alt-altmean).^2/2/sigalt^2).*exp(-(mlon-mlonmean).^2/2/sigmlon^2).*exp(-(mlat-mlatmean).^2/2/sigmlat^2);
-n1=ns(:,:,:,1);
-n1perturb=n1-shapefn*0.25.*n1;
+nsperturb=dat.ns;
+for isp=1:lsp-1
+  for ix2=1:xg.lx(2)
+    amplitude=randn(xg.lx(1),1,xg.lx(3));     %AWGN - note that can result in subtractive effects on density!!!
+    amplitude=0.05*amplitude;                  %amplitude standard dev. is scaled to be 1% of reference profile
 
+    if (ix2>10 && ix2<xg.lx(2)-10)         %do not apply noise near the edge (corrupts boundary conditions)
+      nsperturb(:,ix2,:,isp) = nsperturb(:,ix2,:,isp) + amplitude .* nsperturb(:,ix2,:,isp);
+    end %if
 
-%% Visualize
-ix1=lx1/2;
-
-figure;
-subplot(121);
-imagesc(squeeze(mlon(ix1,1,:)),squeeze(alt(ix1,:,1)),squeeze(n1(ix1,:,:)))
-axis xy;
-
-subplot(122);
-imagesc(squeeze(mlon(ix1,1,:)),squeeze(alt(ix1,:,1)),squeeze(n1perturb(ix1,:,:)))
-axis xy;
-
-
-%% Create a modified density variable for output
-nsperturb=ns;
-nsperturb(:,:,:,1)=n1perturb;
-nsperturb(:,:,:,7)=sum(nsperturb(:,:,:,1:6),4);
+  end %for
+end %for
+nsperturb = max(nsperturb,1e4);                        %enforce a density floor (particularly need to pull out negative densities which can occur when noise is applied)
+nsperturb(:,:,:,lsp) = sum(nsperturb(:,:,:,1:6),4);    %enforce quasineutrality
 
 
 %% WRITE OUT THE RESULTS TO A NEW FILE
@@ -70,3 +52,19 @@ file_format='h5';
 realbits=64;
 writedata(dmy,UTsec,nsperturb,vs1,Ts,outdir,file_format,realbits);
 
+
+%% Visualize
+alt=xg.alt;
+mlat=90-xg.theta*180/pi;
+mlon=xg.phi*180/pi;
+
+ix1=lx1/2;
+
+figure;
+subplot(121);
+imagesc(squeeze(mlon(ix1,1,:)),squeeze(alt(ix1,:,1)),squeeze(dat.ns(ix1,:,:,1)))
+axis xy;
+
+subplot(122);
+imagesc(squeeze(mlon(ix1,1,:)),squeeze(alt(ix1,:,1)),squeeze(nsperturb(ix1,:,:,1)))
+axis xy;
