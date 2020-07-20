8ebb569233def1a2ccf4ff4eee5310d6ac3e8eb8
Matthew Zettergren <mattzett@gmail.com>
2020-07-20 12:24:12 -0400

    working on lowres moore3D test

---
+++ initialize/mooreOK3D_hemis/model_setup_interp_mooreOK3D_lowres.m
@@ -0,0 +1,47 @@
+cwd = fileparts(mfilename('fullpath'));
+gemini_root = [cwd, filesep, '../../../GEMINI'];
+addpath([gemini_root, filesep, 'script_utils'])
+addpath([gemini_root, filesep, 'setup/gridgen'])
+addpath([gemini_root, filesep, '../GEMINI-scripts/setup/gridgen'])
+addpath([gemini_root, filesep, 'setup/'])
+addpath([gemini_root, filesep, 'vis'])
+addpath(['../../setup/gridgen'])
+file_format = 'raw';
+
+%MOORE, OK GRID (FULL)
+dtheta=20;
+dphi=27.5;
+lp=192;
+lq=384;
+lphi=128;
+altmin=80e3;
+glat=39;
+glon=262.51;
+%gridflag=0;
+gridflag=1;
+
+
+%MATLAB GRID GENERATION
+if (~exist('xg'))
+  xg=makegrid_tilteddipole_3D(dtheta,dphi,lp,lq,lphi,altmin,glat,glon,gridflag);
+  %xg=makegrid_tilteddipole_varx2_3D(dtheta,dphi,lp,lq,lphi,altmin,glat,glon,gridflag);
+  %xg=makegrid_tilteddipole_varx2_oneside_3D(dtheta,dphi,lp,lq,lphi,altmin,glat,glon,gridflag);
+end
+
+
+% %PLOT THE GRID IF DESIRED
+% flagsource=0;
+% sourcelat=glat;
+% sourcelong=glon;
+% neugridtype=[];
+% zmin=[];
+% zmax=[];
+% rhomax=[];
+% ha=plotgrid(xg,flagsource,sourcelat,sourcelong,neugridtype,zmin,zmax,rhomax);
+
+
+%SAVE THE GRID DATA
+eqdir='../../../simulations/mooreOK3D_hemis_eq/';
+simID='mooreOK3D_hemis_lowres';
+[nsi,vs1i,Tsi,xgin,ns,vs1,Ts]=eq2dist(eqdir,simID,xg, file_format);
+
