cbe9cbdad774a4d3f00f1efa2baa6ce8420a39f7
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-08-12 04:17:10 -0400

    gdi: matgemini v2.1 api

--- initialize/GDI_periodic_lowres/config.m
+++ initialize/GDI_periodic_lowres/config.m
@@ -1,6 +1,4 @@
 %% coarse grid for testing GDI development
 cwd = fileparts(mfilename('fullpath'));
 
-[cfg, xg] = model_setup(cwd, '~/simulations/gdi_periodic_lowres');
-
-perturb(cfg, xg)
+model_setup(cwd, '~/simulations/gdi_periodic_lowres');
