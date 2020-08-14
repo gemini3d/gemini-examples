fc7fc4938ee3df9ed34e12da891d2782f1c9e5f2
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-08-14 13:24:10 -0400

    {gdi,khi}lowres/config.m complete run and plot example

--- init/KHI_periodic_lowres/config.m
+++ init/KHI_periodic_lowres/config.m
@@ -1,8 +1,10 @@
 %% RISR PERIODIC KHI RUN
 
 cwd = fileparts(mfilename('fullpath'));
-
+out_dir = fullfile(tempdir, 'khi_periodic_lowres');
 params = struct();
 % params.dryrun = true; % setup but not run
 
-gemini3d.gemini_run(cwd, fullfile(tempdir, 'khi_periodic_lowres'), params)
+gemini3d.gemini_run(cwd, out_dir, params)
+
+gemini3d.vis.gemini_plot(out_dir, 'png')
