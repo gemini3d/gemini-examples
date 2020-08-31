5451c7b06c939de52d9463c8fa680aecd888a5b4
Matthew Zettergren <mattzett@gmail.com>
2020-08-31 15:24:54 -0400

    updated ESF example

--- init/ESF/model_setup_interp_ESF.m
+++ init/ESF/model_setup_interp_ESF.m
@@ -1,35 +1,9 @@
-% %% Set paths to other GEMINI repos
-% cwd = fileparts(mfilename('fullpath'));
-% gemini_root = [cwd, filesep, '../../../GEMINI'];
-% addpath([gemini_root, filesep, 'vis'])
-% addpath([gemini_root, filesep, 'script_utils']);
-% addpath([gemini_root, filesep, 'setup/gridgen']);
-% addpath([gemini_root, filesep, '../GEMINI-scripts/setup/gridgen']);
-% addpath([gemini_root, filesep, 'setup']);
-% geminiscripts_root = [cwd, filesep, '../../../GEMINI-scripts'];
-% addpath([geminiscripts_root,filesep,'setup/gridgen']);
-
-
 %% Parameters for creating input files from given equilibrium run
-p.eq_dir='~/zettergmdata/simulations/ESF_eq/';
-p.outdir='~/zettergmdata/simulations/ESF_medres_noise/inputs';
+p.eq_dir='~/simulations/ESF_eq/';
+p.outdir='~/simulations/ESF_medres_noise_test/inputs';
 p.nml='./config.nml';
 p.file_format='h5';
 
-
-%%% Equatorial grid
-%dtheta=5.70;
-%dphi=2.5;
-%lp=192*4;
-%lq=256;
-%lphi=192*2;
-%altmin=80e3;
-%glat=12;
-%glon=360-76.9;     %Jicamarca
-%gridflag=1;
-%flagsource=0;
-%iscurv=true;
-%
 
 %% Equatorial grid
 dtheta=3.5;
@@ -45,14 +19,13 @@ flagsource=0;
 iscurv=true;
 
 
-
 %% MATLAB GRID GENERATION
 if (~exist('xg'))
-    xg=makegrid_tilteddipole_3D(dtheta,dphi,lp,lq,lphi,altmin,glat,glon,gridflag);
+    xg=gemini3d.setup.gridgen.makegrid_tilteddipole_3D(dtheta,dphi,lp,lq,lphi,altmin,glat,glon,gridflag);
 end %if
 lx1=xg.lx(1); lx2=xg.lx(2); lx3=xg.lx(3);
 
 
 %% READ IN AN EXISTING OUTPUT FILE AND DO SOME INTERPOLATION ONTO A NEW GRID
 fprintf('Reading in source file...\n');
-[nsi,vs1i,Tsi]=eq2dist(p,xg);
+[nsi,vs1i,Tsi]=gemini3d.setup.eq2dist(p,xg);
--- init/KHI_periodic_lowres/config.m
+++ init/KHI_periodic_lowres/config.m
@@ -1,10 +1,13 @@
 %% RISR PERIODIC KHI RUN
 
 cwd = fileparts(mfilename('fullpath'));
-out_dir = fullfile(tempdir, 'khi_periodic_lowres');
+%out_dir = fullfile(tempdir, 'khi_periodic_lowres_releasecandidate2');
+out_dir = fullfile('~/simulations/', 'khi_periodic_lowres_releasecandidate2');
 params = struct();
 % params.dryrun = true; % setup but not run
 
-gemini3d.gemini_run(cwd, out_dir, params)
+gemini3d.setup.model_setup(cwd,out_dir)
 
-gemini3d.vis.gemini_plot(out_dir, 'png')
+%setenv('GEMINI_ROOT','~/Projects/GEMINI/')
+%gemini3d.gemini_run(cwd, out_dir, params)
+%gemini3d.vis.gemini_plot(out_dir, 'png')
