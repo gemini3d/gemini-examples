cf2e623e98c10d2e037b876f428165b961f3a12c
Matthew Zettergren <mattzett@gmail.com>
2020-09-09 15:55:24 -0400

    updated lagrangian test

--- init/GDI_periodic_lowres_lagrangian/config.m
+++ init/GDI_periodic_lowres_lagrangian/config.m
@@ -1,10 +1,14 @@
 %% gradient drift instability
-
+setenv('GEMINI_ROOT','~/Projects/GEMINI')
 cwd = fileparts(mfilename('fullpath'));
-out_dir = fullfile(tempdir, 'gdi_periodic_lowres');
+%out_dir = fullfile(tempdir, 'gdi_periodic_lowres_lagrangian');
+out_dir = fullfile('~/simulations/gdi_periodic_lowres_lagrangian');
+
 params = struct();
-% params.dryrun = true; % setup but not run
+params.dryrun = true; % setup but not run
 
 gemini3d.gemini_run(cwd, out_dir, params)
 
-gemini3d.vis.gemini_plot(out_dir, 'png')
+if (~params.dryrun)
+  gemini3d.vis.gemini_plot(out_dir, 'png')
+end
