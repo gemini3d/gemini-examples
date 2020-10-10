343292e74d28791ec769097a0caea3b319c8a2e6
Matthew Zettergren <mattzett@gmail.com>
2020-10-10 13:41:12 -0400

    updating arcs examples again

--- init/GDI_periodic_lowres_lagrangian/config.m
+++ init/GDI_periodic_lowres_lagrangian/config.m
@@ -1,12 +1,12 @@
 %% gradient drift instability
 setenv('GEMINI_ROOT','~/Projects/GEMINI')
-out_dir = fullfile('~/simulations/gdi_periodic_lowres_lagrangian');
+out_dir = fullfile("~/simulations/gdi_periodic_lowres_lagrangian");
 
 
 %% "manual setup"
-cfg = gemini3d.read_config('./config.nml');
-cfg.outdir = gemini3d.fileio.expanduser(out_dir);
-gemini3d.setup.model_setup(cfg)
+%cfg = gemini3d.read_config("./config.nml");
+%cfg.outdir = gemini3d.fileio.expanduser(out_dir);
+gemini3d.setup.model_setup("config.nml",out_dir)
 
 %% OR have matlab run it automatically
 % gemini3d.gemini_run(out_dir, 'dryrun', true)
