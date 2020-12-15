2df16d6e81cedcb9197ad1c8d8325ef81fb9c692
Matthew Zettergren <mattzett@gmail.com>
2020-12-15 10:06:29 -0500

    updating global simulation example

--- init/global_eq/model_setup_global_eq.m
+++ init/global_eq/model_setup_global_eq.m
@@ -1,3 +1,5 @@
+run("../../../mat_gemini/setup.m")
+
 %% A modest resolution grid to test the global run with
 dtheta=15;
 lp=256;
@@ -13,25 +15,34 @@ flagsource=1;
 
 %% MATLAB GRID GENERATION
 if (~exist('xg'))
-    xg= gemini3d.setup.gridgen.makegrid_tilteddipole_3D(dtheta,dphi,lp,lq,lphi,altmin,glat,glon,gridflag);
+    xg= gemini3d.grid.tilted_dipole3d(dtheta,dphi,lp,lq,lphi,altmin,glat,glon,gridflag);
 end %if
 
 
 %% Plot grid
-flagsource=0;
-neuinfo=struct();
-plot_mapgrid(xg,flagsource,neuinfo);
+%flagsource=0;
+%neuinfo=struct();
+%plot_mapgrid(xg,flagsource,neuinfo);
 
 
 %% GENERATE SOME INITIAL CONDITIONS FOR A PARTICULAR EVENT, THESE ACTUALLY DON'T MATTER MUCH SO YOU CAN MAKE UP STUFF
-p.time = datetime(2011,3,11,5,45,0);
+p.times = datetime(2011,3,11,5,45,0);
 p.activ=[120,120,25];
 p.nmf=5e11;
 p.nme=2e11;
-[ns,Ts,vsx1]= gemini3d.setup.eqICs3D(p,xg);    %note that this actually calls msis_matlab - should be rewritten to include the neutral module form the fortran code!!!
+[ns,Ts,vsx1]= gemini3d.model.eqICs(p,xg);    %note that this actually calls msis_matlab - should be rewritten to include the neutral module form the fortran code!!!
 
 
 %% WRITE THE GRID AND INITIAL CONDITIONS
-p.simdir='../../../simulations/input/global_eq';
+p.simdir="~/simulations/global_eq_testing";
+p.indat_grid=strcat(p.simdir,"/inputs/simgrid.h5");
+p.indat_size=strcat(p.simdir,"/inputs/simsize.h5");
+p.outdir=strcat(p.simdir);
 gemini3d.write.grid(p,xg);
-gemini3d.write.data(p.time,ns,vsx1,Ts,p.simdir);
+filename=strcat(p.simdir,"/inputs/initial_conditions.h5");
+gemini3d.write.data(p.times,ns,vsx1,Ts,filename);
+
+
+%% Copy over the config.nml
+system(strcat("cp config.nml ",p.simdir,"/inputs/"));
+
