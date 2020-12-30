74d8d542c711e2e322d5d45c078da68df767d27a
Michael Hirsch <scivision@users.noreply.github.com>
2020-12-29 23:47:13 -0500

    perturb: writes file

--- init/GDI_periodic_lowres/perturb.py
+++ init/GDI_periodic_lowres/perturb.py
@@ -16,16 +16,17 @@ def perturb(cfg: T.Dict[str, T.Any], xg: T.Dict[str, T.Any]):
     x1 = xg["x1"][2:-2]
     x2 = xg["x2"][2:-2]
     # %% LOAD THE FRAME OF THE SIMULATION THAT WE WANT TO PERTURB
-    dat = gemini3d.read.state(cfg["indat_file"])
-    lsp = dat["ns"].shape[0]
+    dat = gemini3d.read.data(cfg["indat_file"], var=["ns", "Ts", "vs1"])
+    ns = dat["ns"][1]
+    lsp = ns.shape[0]
 
     # %% Choose a single profile from the center of the eq domain
     ix2 = xg["lx"][1] // 2
     ix3 = xg["lx"][2] // 2
-    nsscale = np.zeros(dat["ns"].shape)
+    nsscale = np.zeros(ns.shape)
     for i in range(lsp):
-        nprof = dat["ns"][i, :, ix2, ix3]
-        nsscale[i, :, :, :] = nprof
+        nprof = ns[i, :, ix2, ix3]
+        nsscale[i, :, :, :] = nprof[:, None, None]
 
     # %% GDI EXAMPLE (PERIODIC) INITIAL DENSITY STRUCTURE AND SEEDING
     ell = 5e3  # gradient scale length for patch/blob
@@ -33,33 +34,28 @@ def perturb(cfg: T.Dict[str, T.Any], xg: T.Dict[str, T.Any]):
     x22 = -45e3  # other patch edge
     nepatchfact = 10  # density increase factor over background
 
-    nsperturb = np.zeros(dat["ns"].shape)
+    nsperturb = np.zeros(ns.shape)
     for i in range(lsp - 1):
         for j in range(xg["lx"][1]):
-            amplitude = numpy.random.standard_normal([xg["lx"][0], 1, xg["lx"][2]])
+            amplitude = numpy.random.standard_normal([xg["lx"][0], xg["lx"][2]])
             # AWGN - note that can result in subtractive effects on density so apply a floor later!!!
             amplitude = 0.01 * amplitude
             # amplitude standard dev. is scaled to be 1% of reference profile
 
-            nsperturb[i, :, j, :] = nsscale[
-                i, :, j, :
-            ] + nepatchfact * nsscale[  # original data
-                i, :, j, :
-            ] * (
-                1 / 2 * np.tanh((x2[j] - x21) / ell)
-                - 1 / 2 * np.tanh((x2[j] - x22) / ell)
+            # original data
+            nsperturb[i, :, j, :] = nsscale[i, :, j, :] + nepatchfact * nsscale[i, :, j, :] * (
+                1 / 2 * np.tanh((x2[j] - x21) / ell) - 1 / 2 * np.tanh((x2[j] - x22) / ell)
             )
             # patch, note offset in the x2 index!!!!
 
-            if (j > 10) & (j < xg["lx"][1] - 10):
+            if (j > 10) and (j < xg["lx"][1] - 10):
                 # do not apply noise near the edge (corrupts boundary conditions)
-                nsperturb[i, :, j, :] = (
-                    nsperturb[i, :, j, :] + amplitude * nsscale[i, :, j, :]
-                )
+                nsperturb[i, :, j, :] = nsperturb[i, :, j, :] + amplitude * nsscale[i, :, j, :]
 
     nsperturb = np.maximum(nsperturb, 1e4)
-    # enforce a density floor (particularly need to pull out negative densities which can occur when noise is applied)
-    nsperturb[lsp, :, :, :] = nsperturb[:-1, :, :, :].sum(axis=0)
+    # enforce a density floor (particularly need to pull out negative densities
+    # which can occur when noise is applied)
+    nsperturb[-1, :, :, :] = nsperturb[:-1, :, :, :].sum(axis=0)
     # enforce quasineutrality
 
     # %% KILL OFF THE E-REGION WHICH WILL DAMP THE INSTABILITY (AND USUALLY ISN'T PRESENT IN PATCHES)
@@ -72,7 +68,7 @@ def perturb(cfg: T.Dict[str, T.Any], xg: T.Dict[str, T.Any]):
             for ix2 in range(xg["lx"][1]):
                 nsperturb[i, :, ix2, ix3] = 1e6 + nsperturb[i, :, ix2, ix3] * taper
 
-    nsperturb[lsp, :, :, :] = nsperturb[:-1, :, :, :].sum(axis=0)
+    nsperturb[-1, :, :, :] = nsperturb[:-1, :, :, :].sum(axis=0)
     # enforce quasineutrality
 
     # %% WRITE OUT THE RESULTS TO the same file
@@ -80,7 +76,7 @@ def perturb(cfg: T.Dict[str, T.Any], xg: T.Dict[str, T.Any]):
         cfg["indat_file"],
         dat["time"],
         ns=nsperturb,
-        vs=dat["vs1"],
-        Ts=dat["Ts"],
+        vs=dat["vs1"][1],
+        Ts=dat["Ts"][1],
         file_format=cfg["file_format"],
     )
