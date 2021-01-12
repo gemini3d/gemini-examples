4169792319a717842c790291d358fbdded08f78c
Michael Hirsch <scivision@users.noreply.github.com>
2021-01-11 23:00:51 -0500

    setup: clone recursively

--- setup.m
+++ setup.m
@@ -1,10 +1,13 @@
 function setup()
 %% configure paths to work with MatGemini
-narginchk(0,0)
+
 cwd = fileparts(mfilename('fullpath'));
-gemini_matlab = fullfile(cwd, '../mat_gemini');
+gemini_matlab = getenv('MATGEMINI');
+if isempty(gemini_matlab)
+  gemini_matlab = fullfile(cwd, '../mat_gemini');
+end
 if ~isfolder(gemini_matlab)
-  cmd = ['git -C ', fullfile(cwd, '..'), ' clone https://github.com/gemini3d/mat_gemini'];
+  cmd = "git -C " + fullfile(cwd, '..') + " clone --recurse-submodules https://github.com/gemini3d/mat_gemini";
   disp(cmd)
   ret = system(cmd);
   assert(ret==0, 'problem downloading Gemini Matlab functions')
