5bc28d3533e54304d969124d5e9d5ecad15f2b08
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2021-01-20 12:44:13 -0500

    setup json meta

--- setup.m
+++ setup.m
@@ -2,16 +2,24 @@ function setup()
 %% configure paths to work with MatGemini
 
 cwd = fileparts(mfilename('fullpath'));
+meta = jsondecode(fileread(fullfile(cwd, "libraries.json")));
+
 gemini_matlab = getenv('MATGEMINI');
 if isempty(gemini_matlab)
   gemini_matlab = fullfile(cwd, '../mat_gemini');
 end
+
 if ~isfolder(gemini_matlab)
-  cmd = "git -C " + fullfile(cwd, '..') + " clone --recurse-submodules https://github.com/gemini3d/mat_gemini";
-  disp(cmd)
+  cmd = "git -C " + fullfile(cwd, '..') + " clone --recurse-submodules " + meta.matgemini.git;
   ret = system(cmd);
-  assert(ret==0, 'problem downloading Gemini Matlab functions')
+
+  if ret == 0 && ~isempty(meta.matgemini.tag)
+    ret = system("git -C " + gemini_matlab + " checkout " + meta.matgemini.tag);
+  end
+
+  assert(ret==0, "Failed to download MatGemini")
 end
+
 run(fullfile(gemini_matlab, 'setup.m'))
 
 end
