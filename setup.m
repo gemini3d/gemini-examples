6c989555bf95d5d4859148a1d2cd80ca70379c97
Michael Hirsch <scivision@users.noreply.github.com>
2021-02-18 11:40:29 -0500

    update setup.m tag

--- setup.m
+++ setup.m
@@ -1,25 +1,29 @@
 function setup()
 %% configure paths to work with MatGemini
 
-cwd = fileparts(mfilename('fullpath'));
-meta = jsondecode(fileread(fullfile(cwd, "libraries.json")));
+cwd = fileparts(mfilename("fullpath"));
 
-gemini_matlab = getenv('MATGEMINI');
+
+gemini_matlab = getenv("MATGEMINI");
 if isempty(gemini_matlab)
-  gemini_matlab = fullfile(cwd, '../mat_gemini');
+  gemini_matlab = fullfile(cwd, "../mat_gemini");
 end
 
-if ~isfolder(gemini_matlab)
-  cmd = "git -C " + fullfile(cwd, '..') + " clone --recurse-submodules " + meta.matgemini.git;
+setup_file = fullfile(gemini_matlab, "setup.m");
+
+if ~isfile(setup_file)
+  meta = jsondecode(fileread(fullfile(cwd, "libraries.json")));
+
+  cmd = "git -C " + fullfile(cwd, "..") + " clone --recurse-submodules " + meta.matgemini.git;
   ret = system(cmd);
 
-  if ret == 0 && ~isempty(meta.matgemini.tag)
+  if ret == 0 && isfield(meta.matgemini, "tag") && ~isempty(meta.matgemini.tag)
     ret = system("git -C " + gemini_matlab + " checkout " + meta.matgemini.tag);
   end
 
   assert(ret==0, "Failed to download MatGemini")
 end
 
-run(fullfile(gemini_matlab, 'setup.m'))
+run(setup_file)
 
 end
