212fbd2a0364c7057078ede76495683b8f852147
Michael Hirsch <scivision@users.noreply.github.com>
2021-03-08 23:32:05 -0500

    use download fixture

--- cmake/download_extract.cmake
+++ cmake/download_extract.cmake
@@ -1,14 +1,16 @@
+cmake_minimum_required(VERSION 3.18...3.20)
+
 include(${CMAKE_CURRENT_LIST_DIR}/parse_nml.cmake)
 
-function(download_extract nml_file)
+function(download_extract nml_file eq_dir)
 
-parse_nml("${nml_file}" "eq_url")
+parse_nml(${nml_file} "eq_url")
 if(NOT eq_url)
   message(STATUS "SKIP: ${name}: ${nml_file} does not define eq_url, and ${eq_dir} is not a directory.")
   return()
 endif()
 
-parse_nml("${nml_file}" "eq_zip")
+parse_nml(${nml_file} "eq_zip")
 if(NOT eq_zip)
   get_filename_component(eq_zip ${eq_dir} NAME)
   set(eq_zip ${eq_zip}.zip)
@@ -20,12 +22,10 @@ if(NOT EXISTS ${eq_zip})
   file(DOWNLOAD ${eq_url} ${eq_zip} TLS_VERIFY ON)
 endif()
 
-message(STATUS "${eq_zip} => ${eq_dir}")
 get_filename_component(eq_root ${eq_dir} DIRECTORY)
-
+message(STATUS "${eq_zip} => ${eq_root}")
 file(ARCHIVE_EXTRACT INPUT ${eq_zip} DESTINATION ${eq_root})
 
 endfunction(download_extract)
 
-
-# download_extract("${nml_file}")
+download_extract(${nml_file} ${eq_dir})
--- cmake/parse_nml.cmake
+++ cmake/parse_nml.cmake
@@ -1,7 +1,7 @@
 function(parse_nml nml_file var)
 # get alphanumeric variable such as a file path from Fortran namelist .nml file
 
-file(STRINGS "${nml_file}" m REGEX "${var}[ ]*=[ ]*\'?\"?([~/:\.\?\&=A-Za-z0-9_]+)" LIMIT_COUNT 1)
+file(STRINGS ${nml_file} m REGEX "${var}[ ]*=[ ]*\'?\"?([~/:\.\?\&=A-Za-z0-9_]+)" LIMIT_COUNT 1)
 if(NOT m)
   set(${var} PARENT_SCOPE)
   return()
