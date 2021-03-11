fde5e3488838a97be090f343425832903feaba56
Michael Hirsch <scivision@users.noreply.github.com>
2021-03-11 00:37:48 -0500

    labels

--- cmake/EquilSetup.cmake
+++ cmake/EquilSetup.cmake
@@ -4,7 +4,7 @@ if(NOT ${in_dir} IN_LIST equil_dirs)
   add_test(NAME "setup:download_eq:${name}"
     COMMAND ${CMAKE_COMMAND} -Din_dir:PATH=${in_dir} -Dname=${name} -Dout_dir:PATH=${out_dir} -P ${CMAKE_CURRENT_LIST_DIR}/cmake/download_eq.cmake)
   set_tests_properties("setup:download_eq:${name}" PROPERTIES
-    LABELS "setup;download;${label}"
+    LABELS "download;${label}"
     REQUIRED_FILES ${in_dir}/config.nml
     TIMEOUT 300
     FIXTURES_SETUP ${name}:eq_fxt)
--- cmake/GeminiCompare.cmake
+++ cmake/GeminiCompare.cmake
@@ -7,6 +7,7 @@ add_test(NAME compare:download:${name}
 set_tests_properties(compare:download:${name} PROPERTIES
   FIXTURES_SETUP ${name}:compare_fxt
   FIXTURES_REQUIRED ${name}:run_fxt
+  LABELS "download;${label}"
   TIMEOUT 180)
 
 set(compare_cmd ${compare_exe} ${out_dir} ${ref_dir})
