06d27af384dacd94d5149a5a2ea3946df65435d9
Michael Hirsch <scivision@users.noreply.github.com>
2021-03-17 13:21:20 -0400

    package: use Zstd compression

--- cmake/GeminiPackage.cmake
+++ cmake/GeminiPackage.cmake
@@ -6,16 +6,16 @@ set(upload_root ${GEMINI_SIMROOT}/upload-${ts})
 
 file(MAKE_DIRECTORY ${upload_root})
 
-set(new_zip ${upload_root}/${name}.zip)
-set(out_dir ${GEMINI_SIMROOT}/${name})
+set(archive ${upload_root}/${name}.zstd)
+set(data_dir ${GEMINI_SIMROOT}/${name})
 
 add_test(NAME "package:${name}"
-  COMMAND ${CMAKE_COMMAND} -Dout_dir:PATH=${out_dir} -Dnew_zip:FILEPATH=${new_zip} -P ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/zip.cmake)
+  COMMAND ${CMAKE_COMMAND} -Din:PATH=${data_dir} -Dout:FILEPATH=${archive} -P ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/zip.cmake)
 
 set_tests_properties("package:${name}" PROPERTIES
   FIXTURES_REQUIRED ${name}:package_fxt
   LABELS "package;${label}"
-  REQUIRED_FILES "${out_dir}/inputs/config.nml;${out_dir}/output.nml"
+  REQUIRED_FILES "${data_dir}/inputs/config.nml;${data_dir}/output.nml"
   TIMEOUT 120)
 
 endfunction(gemini_package)
--- cmake/zip.cmake
+++ cmake/zip.cmake
@@ -1,3 +1,24 @@
-file(ARCHIVE_CREATE OUTPUT ${new_zip}
-  PATHS ${out_dir}
-  FORMAT zip)
+function(make_archive in out)
+
+get_filename_component(in ${in} ABSOLUTE)
+get_filename_component(out ${out} ABSOLUTE)
+
+file(ARCHIVE_CREATE
+  OUTPUT ${out}
+  PATHS ${in}
+  COMPRESSION Zstd
+  COMPRESSION_LEVEL 3)
+
+# ensure a file was created (weak validation)
+if(NOT EXISTS ${out})
+  message(FATAL_ERROR "Archive ${out} was not created.")
+endif()
+
+file(SIZE ${out} fsize)
+if(fsize LESS 1000)
+  message(FATAL_ERROR "Archive ${out} may be malformed.")
+endif()
+
+endfunction(make_archive)
+
+make_archive(${in} ${out})
