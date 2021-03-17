f590e3d503a7c7112afb950854379941e2049a68
Michael Hirsch <scivision@users.noreply.github.com>
2021-03-17 14:02:41 -0400

    package: need command line form of tar to avoid computer-specific relative paths

--- cmake/zip.cmake
+++ cmake/zip.cmake
@@ -3,11 +3,19 @@ function(make_archive in out)
 get_filename_component(in ${in} ABSOLUTE)
 get_filename_component(out ${out} ABSOLUTE)
 
-file(ARCHIVE_CREATE
-  OUTPUT ${out}
-  PATHS ${in}
-  COMPRESSION Zstd
-  COMPRESSION_LEVEL 3)
+# not usable due to internal paths always relative to PROJECT_BINARY_DIR
+# https://gitlab.kitware.com/cmake/cmake/-/issues/21653
+# file(ARCHIVE_CREATE
+#   OUTPUT ${out}
+#   PATHS ${in}
+#   COMPRESSION Zstd
+#   COMPRESSION_LEVEL 3)
+
+# need working_directory ${in} to avoid computer-specific relative paths
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -E tar c ${out} --zstd -- ${in}
+  WORKING_DIRECTORY ${in}
+)
 
 # ensure a file was created (weak validation)
 if(NOT EXISTS ${out})
