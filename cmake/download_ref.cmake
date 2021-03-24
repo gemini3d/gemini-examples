65ca56c0e5e199fd23975f9ea4635d70d924e8ba
Michael Hirsch <scivision@users.noreply.github.com>
2021-03-24 19:57:04 -0400

    md5 checking

--- cmake/download_ref.cmake
+++ cmake/download_ref.cmake
@@ -16,21 +16,44 @@ set(archive ${ref_root}/${archive_name})
 
 # check if extracted data exists
 if(IS_DIRECTORY ${ref_dir})
-  return()
+  if(md5 AND EXISTS ${ref_dir}/md5sum.txt)
+    file(READ ${ref_dir}/md5sum.txt extracted_md5)
+    if(${extracted_md5} STREQUAL ${md5})
+      return()
+    else()
+      message(STATUS "${name}: extracted md5 ${extracted_md5} != JSON md5 ${md5}")
+    endif()
+  else()
+    return()
+  endif()
 endif()
 
 # check if archive available
-if(NOT EXISTS ${archive})
+set(hash_ok true)
+if(EXISTS ${archive} AND DEFINED md5)
+  file(MD5 ${archive} archive_md5)
+  if(NOT ${archive_md5} STREQUAL ${md5})
+    message(STATUS "${name}: archive md5 ${archive_md5} != JSON md5 ${md5}")
+    set(hash_ok false)
+  endif()
+endif()
+
+if(NOT EXISTS ${archive} OR NOT hash_ok)
   message(STATUS "DOWNLOAD: ${url} => ${archive}")
-  file(DOWNLOAD ${url} ${archive} TLS_VERIFY ON)
+  if(md5)
+    file(DOWNLOAD ${url} ${archive} TLS_VERIFY ON EXPECTED_HASH MD5=${md5})
+  else()
+    file(DOWNLOAD ${url} ${archive} TLS_VERIFY ON)
+  endif()
 endif()
 
 
 message(STATUS "EXTRACT: ${archive} => ${ref_dir}")
 file(ARCHIVE_EXTRACT INPUT ${archive} DESTINATION ${ref_dir})
 
-file(MD5 ${archive} _md5)
-file(WRITE ${ref_dir}/md5sum.txt ${_md5})
+# to compare extracted contents and auto-update ref data
+file(MD5 ${archive} archive_md5)
+file(WRITE ${ref_dir}/md5sum.txt ${archive_md5})
 
 endfunction(download_ref)
 
