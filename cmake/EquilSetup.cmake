54af2d6a0fa4f4747fa3a6fc38b47afbe58c1ec8
Michael Hirsch <scivision@users.noreply.github.com>
2021-04-02 10:41:02 -0400

    check that nml parsed

--- cmake/EquilSetup.cmake
+++ cmake/EquilSetup.cmake
@@ -1,10 +1,17 @@
-function(equil_setup in_dir out_dir label)
+function(equil_setup in_dir out_dir name label)
 
 cmake_path(APPEND nml_file ${in_dir} config.nml)
 
 parse_nml(${nml_file} "eq_dir" "path")
-
+if(NOT eq_dir)
+  message(FATAL_ERROR "${name}: missing eq_dir in ${nml_file}")
+endif()
 cmake_path(GET eq_dir FILENAME eq_name)
+if(NOT eq_name)
+  message(FATAL_ERROR "${name}: ${eq_dir} seems malformed, could not get directory name.")
+endif()
+
+
 
 add_test(NAME "setup:download_eq:${name}"
   COMMAND ${CMAKE_COMMAND} -Deq_dir:PATH=${eq_dir} -Dname=${name} -P ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/download_eq.cmake)
