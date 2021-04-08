aef8f602d08c1e995dd95b005bcd15211c387abd
Michael Hirsch <scivision@users.noreply.github.com>
2021-04-08 10:07:12 -0400

    add upload

--- cmake/GeminiPackage.cmake
+++ cmake/GeminiPackage.cmake
@@ -5,18 +5,34 @@ file(MAKE_DIRECTORY ${upload_root})
 set(ARC_TYPE zstd)
 
 
-function(gemini_package GEMINI_SIMROOT name label)
+function(gemini_package GEMINI_SIMROOT out_dir name label)
 
 cmake_path(APPEND archive ${upload_root} ${name}.${ARC_TYPE})
 cmake_path(APPEND data_dir ${GEMINI_SIMROOT} ${name})
 
-add_test(NAME "package:${name}"
+add_test(NAME "package:archive:${name}"
   COMMAND ${CMAKE_COMMAND} -Din:PATH=${data_dir} -Dout:FILEPATH=${archive} -P ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/archive.cmake)
 
-set_tests_properties("package:${name}" PROPERTIES
+set_tests_properties("package:archive:${name}" PROPERTIES
   FIXTURES_REQUIRED ${name}:package_fxt
+  FIXTURES_SETUP ${name}:upload_fxt
   LABELS "package;${label}"
   REQUIRED_FILES "${data_dir}/inputs/config.nml;${data_dir}/output.nml"
   TIMEOUT 120)
+
+find_program(rclone NAMES rclone)
+
+if(rclone)
+
+add_test(NAME "package:upload:${name}"
+  COMMAND ${CMAKE_COMMAND} -Darchive:FILEPATH=${archive} -Dout_dir:PATH=${out_dir} -Dname=${name} -Dupload_root:PATH=gemini_upload-${ts} -P ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/upload.cmake)
+
+set_tests_properties("package:upload:${name}" PROPERTIES
+  FIXTURES_REQUIRED ${name}:upload_fxt
+  LABELS "package;${label}"
+  REQUIRED_FILES ${archive}
+  TIMEOUT 300)
+
+endif(rclone)
 
 endfunction(gemini_package)
---
+++ cmake/upload.cmake
@@ -0,0 +1,16 @@
+set(PACKAGE_REMOTE dropbox)
+
+function(upload_package archive out_dir name upload_root)
+# NOTE: rclone copy default does not overwrite
+
+# upload archive itself
+execute_process(COMMAND rclone copy ${archive} ${PACKAGE_REMOTE}:${upload_root})
+
+# upload plots directory to avoid needing to extract on local computers
+# that is for others to quickly preview plots
+execute_process(COMMAND rclone copy ${out_dir}/plots ${PACKAGE_REMOTE}:${upload_root}/plots/${name})
+
+endfunction(upload_package)
+
+
+upload_package(${archive} ${out_dir} ${name} ${upload_root})
