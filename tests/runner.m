92d11b6b796aeb3a94e000788d289702fabf2ad9
Michael Hirsch <scivision@users.noreply.github.com>
2021-05-12 16:53:21 -0400

    cleanup tests

--- tests/runner.m
+++ tests/runner.m
@@ -2,7 +2,7 @@ function runner(tc, name, outdir)
 arguments
   tc (1,1) matlab.unittest.TestCase
   name (1,1) string
-  outdir string = fullfile(tempdir, name)
+  outdir (1,1) string
 end
 
 cwd = fileparts(mfilename('fullpath'));
@@ -10,17 +10,14 @@ cwd = fileparts(mfilename('fullpath'));
 test_dir = fullfile(cwd, "../init", name);
 %% setup new test data
 p = gemini3d.read.config(test_dir);
-tc.verifyNotEmpty(p, "did not contain config.nml")
-p.out_dir = outdir;
+tc.assumeNotEmpty(p, "did not contain config.nml")
+p.outdir = outdir;
 
 try
   gemini3d.model.setup(p);
 catch e
-  if ~contains(e.identifier, 'file_not_found')
-    rethrow(e)
-  end
-  fprintf(2, 'SKIP %s\n', name);
-  return
+  tc.assumeSubstring(e.identifier, 'file_not_found')
+  rethrow(e)
 end
 
 end  % function
