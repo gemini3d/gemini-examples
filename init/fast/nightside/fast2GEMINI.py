6de769444f509749147de3bf1f7494eaab7035f0
Matthew D. Zettergren <mattzett@gmail.com>
2021-09-13 11:50:23 -0400

    adding input from FAST data

---
+++ init/fast/nightside/fast2GEMINI.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 13 11:47:40 2021
+
+Create GEMINI precipitation input from FAST data
+
+@author: zettergm
+"""
+
+# global vars
+filename="/Users/zettergm/Dropbox (Personal)/proposals/UNH_GDC/FASTdata/nightside.txt"
+
+def fast2GEMINI():
+    
