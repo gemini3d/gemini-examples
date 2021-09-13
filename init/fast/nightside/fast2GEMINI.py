ed4d0050f74c5a460154e7b29f7879168d5a8ebf
Matthew D. Zettergren <mattzett@gmail.com>
2021-09-13 11:55:00 -0400

    clean out old files

--- init/fast/nightside/fast2GEMINI.py
+++ init/fast/nightside/fast2GEMINI.py
@@ -8,8 +8,20 @@ Create GEMINI precipitation input from FAST data
 @author: zettergm
 """
 
+# imports
+from fast import readfast,smoothfast
+
+
 # global vars
 filename="/Users/zettergm/Dropbox (Personal)/proposals/UNH_GDC/FASTdata/nightside.txt"
 
 def fast2GEMINI():
+    # read in the data
+    [invlat,eflux,chare]=readfast(filename)
+    
+    # smooth data a bit prior to insertion into model
+    lsmooth=3
+    [efluxsmooth,charesmooth]=smoothfast(lsmooth,eflux,chare)
+    
+    # now construct space-time evoluation from these data
     
