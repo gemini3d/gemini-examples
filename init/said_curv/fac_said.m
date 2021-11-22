1640774c4f3dffed9303509e66202ff57f9f6bbf
Michael Hirsch <scivision@users.noreply.github.com>
2021-11-22 16:55:37 -0500

    said_curv: fac_said.m based on fac_said.py

--- init/said_curv/fac_said.m
+++ init/said_curv/fac_said.m
@@ -1,4 +1,4 @@
-function E=fac_said(E, Nt, gridflag, flagdip)
+function E = fac_said(E, Nt, gridflag, flagdip)
 % Set the top boundary shape (current density) and potential solve type flag
 arguments
   E (1,1) struct
@@ -7,28 +7,27 @@ arguments
   flagdip (1,1) logical = false %#ok<INUSA>
 end
 
+if E.llon == 1 || E.llat == 1
+  error("Efield:fac_said is for 3D sims only")
+end
 
-if E.llon>1
-  shapelon=exp(-(E.MLON - E.mlonmean).^2/2 / E.mlonsig^2);
+%% nonuniform in longitude
+shapelon = exp(-((E.MLON - E.mlonmean).^2) / 2 / E.mlonsig^2);
+
+%% nonuniform in latitude
+shapelat = 0.7*exp(-((E.MLAT - E.mlatmean + 1.5 * E.mlatsig).^2) / 2 / E.mlatsig^2) ...
+             - exp(-((E.MLAT - E.mlatmean - 1.5 * E.mlatsig).^2) / 2 / E.mlatsig^2);
+
+for i = 3:Nt
+%could have different boundary types for different times if the user wanted...
+E.flagdirich(i)=0;
+
+if gridflag==1
+  k = "Vminx1it";
 else
-  % doing a simulation with lat/alt. slice probably
-  shapelon=1;
+  k = "Vmaxx1it";
 end
 
-if E.llat>1
-  shapelat = exp(-(E.MLAT - E.mlatmean - 1.5 * E.mlatsig).^2/ 2 / E.mlatsig^2) - exp(-(E.MLAT - E.mlatmean + 1.5 * E.mlatsig).^2/ 2 / E.mlatsig^2);
-else
-  shapelat = 1;
-end
+E.(k)(:,:,i) = E.Jtarg .* shapelon .* shapelat;
 
-for i = 6:Nt
-  %could have different boundary types for different times if the user wanted...
-  E.flagdirich(i)=0;
-  if gridflag==1
-    E.Vminx1it(:,:,i) = E.Jtarg.*shapelon.*shapelat;
-  else
-    E.Vmaxx1it(:,:,i) = E.Jtarg.*shapelon.*shapelat;
-  end
 end
-
-end % function
