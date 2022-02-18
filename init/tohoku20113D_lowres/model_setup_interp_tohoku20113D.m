e6dd134238a53cffa377d499604775ab8a5e284e
Matthew Zettergren <zettergm@Matthews-Mac-Pro.local>
2022-02-18 12:58:37 -0500

    smaller GDI example based on MR research

--- init/tohoku20113D_lowres/model_setup_interp_tohoku20113D.m
+++ init/tohoku20113D_lowres/model_setup_interp_tohoku20113D.m
@@ -1,15 +1,15 @@
 %A MEDIUM RES TOHOKU
 p.dtheta=7.5;
 p.dphi=12;
-p.lp=128;
+p.lp=192;
 p.lq=512;
-p.lphi=128;
+p.lphi=192;
 p.altmin=80e3;
 p.glat=42.45;
 p.glon=143.4;
 p.gridflag=1;
 p.flagsource=1;
-p.eq_dir='~/simulations/tohoku3d_eq/';
+p.eq_dir='~/simulations/raid/tohoku3d_eq/';
 p.nml='./config.ini';
 
 %RUN THE GRID GENERATION CODE
@@ -18,9 +18,10 @@ if ~exist('xg', 'var')
 end
 %ALTERNATIVELY WE MAY WANT TO READ IN AN EXISTING OUTPUT FILE AND DO SOME INTERPOLATION ONTO A NEW GRID
 
-p.outdir= '~/simulations/tohoku20113D_lowres_IVV';
+p.outdir= '~/simulations/raid/tohoku20113D_lowres_IVV';
 p.indat_grid=[p.outdir,'/inputs/simgrid.h5'];
 p.indat_size=[p.outdir,'/inputs/simsize.h5'];
 p.indat_file=[p.outdir,'/inputs/initial_conditions.h5'];
 p.file_format='h5';
 dat = gemini3d.model.eq2dist(p,xg);
+system(['cp ./config.nml ',p.outdir,'/inputs']);
