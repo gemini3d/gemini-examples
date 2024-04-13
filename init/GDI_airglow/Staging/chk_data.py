e716a83ddba3310c2be69d364a52f54b1a0012c5
Matthew Zettergren <zettergm@Matthews-MacBook-Pro.local>
2024-04-13 09:46:10 -0400

    start work on airglow initialization of patch in model

---
+++ init/GDI_airglow/Staging/chk_data.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 12 13:28:43 2024
+
+@author: zettergm
+"""
+
+import numpy as np
+import h5py
+
+f=h5py.File("./AGP1_outline.h5",mode="r")
+
+#f.close()
